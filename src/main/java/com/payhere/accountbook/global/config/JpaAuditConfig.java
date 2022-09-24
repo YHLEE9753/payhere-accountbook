e61e1d31d149821a295ec00869fc3995386be89d
Yonghoon Lee <dldydgns53@naver.com>
2022-09-24 23:30:15 +0900

    feat: Audit 및 JsonDateTime config 추가

---
+++ src/main/java/com/payhere/accountbook/global/config/JpaAuditConfig.java
@@ -0,0 +1,34 @@
+package com.payhere.accountbook.global.config;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+
+	@Bean
+	public AuditorAware<Long> auditorAware() {
+		return () -> {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+			if (authentication == null || !authentication.isAuthenticated() || isAnonymous(authentication)) {
+				return Optional.of(0L);
+			}
+
+			Long memberId = Optional.ofNullable((Long)authentication.getPrincipal()).orElse(0L);
+			return Optional.of(memberId);
+		};
+	}
+
+	private boolean isAnonymous(Authentication authentication) {
+		return authentication instanceof AnonymousAuthenticationToken;
+	}
+}
