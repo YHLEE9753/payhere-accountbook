45f57d3ba9be6184d4c54cdbbd6494e0e6c989c1
Yonghoon Lee <dldydgns53@naver.com>
2022-09-24 23:29:40 +0900

    feat: JPA BaseEntity 작성

---
+++ src/main/java/com/payhere/accountbook/global/base/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.payhere.accountbook.global.base;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity extends BaseTimeEntity {
+
+	@CreatedBy
+	@Column(name = "created_by", length = 40, nullable = false, updatable = false)
+	private Long createdBy;
+
+	@LastModifiedBy
+	@Column(name = "updated_by", length = 40, nullable = false)
+	private Long updatedBy;
+}
